92ba721cfc7238da8e482e47f953250f1ee1fc5e
kayesokua <kaye.sokua@code.berlin>
2023-03-23 23:34:07 +0100

    Refactored Merge Class

--- challenge2/merge.py
+++ challenge2/merge.py
@@ -1,53 +1,79 @@
-class MergeRequest:
+STATUS = {
+    "OPEN": "open",
+    "CLOSED": "closed",
+    "REJECTED": "rejected",
+    "APPROVED": "approved",
+    "PENDING": "pending",
+}
 
+MESSAGES = {
+    "UPVOTE": "upvote",
+    "DOWNVOTE": "downvote",
+    "CLOSED": "Merge request has been approved and closed",
+    "REJECTED": "Merge request has been rejected and closed",
+    "PENDING": "Cannot close merge request until it has been approved or rejected",
+    "NO_VOTES": "No votes yet",
+    "VOTES": "{} upvotes, {} downvotes",
+    "VOTES_UP": "{} upvotes",
+    "VOTES_DOWN": "{} downvotes",
+    "INVALID_VOTE": "not correct type",
+    "INVALID_STATUS": "can't vote on a closed merge request",
+}
+
+class MergeRequest:
     def __init__(self):
         self._context = {"upvotes": set(), "downvotes": set()}
-        self._status = "open"
+        self._status = STATUS["OPEN"]
 
     def status(self):
-        if self._status == "closed":
+        if self._status == STATUS["CLOSED"]:
             return self._status
+        elif self._context["downvotes"]:
+            return STATUS["REJECTED"]
+        elif len(self._context["upvotes"]) >= 2:
+            return STATUS["APPROVED"]
         else:
-            if self._context["downvotes"]:
-                return "rejected"
-            else:
-                if len(self._context["upvotes"]) >= 2:
-                    return "approved"
-                else:
-                    return "pending"
-        return None
+            return STATUS["PENDING"]
 
-    def vote(self, by_user, type):
-        if self._status == "closed":
+    def vote(self, by_user, vote_type):
+        if self._status == STATUS["CLOSED"]:
             return "can't vote on a closed merge request"
-
-        if type == "downvote":
+        if vote_type == "downvote":
             self._context["upvotes"].discard(by_user)
             self._context["downvotes"].add(by_user)
-        elif type == "upvote":
+        elif vote_type == "upvote":
             self._context["downvotes"].discard(by_user)
             self._context["upvotes"].add(by_user)
         else:
-            return "not correct type"
+            return MESSAGES["INVALID_VOTE"]
 
     def close(self):
-        if self.status() == "approved":
-            self._status = "closed"
-            return "Merge request has been approved and closed"
-        elif self.status() == "rejected":
-            self._status = "closed"
-            return "Merge request has been rejected and closed"
+        if self.status() == STATUS["APPROVED"]:
+            self._status = STATUS["CLOSED"]
+            return MESSAGES["CLOSED"]
+        elif self.status() == STATUS["REJECTED"]:
+            self._status = STATUS["CLOSED"]
+            return MESSAGES["REJECTED"]
         else:
-            return "Cannot close merge request until it has been approved or rejected"
+            return MESSAGES["PENDING"]
 
     def getvotes(self):
         upvotes = len(self._context["upvotes"])
         downvotes = len(self._context["downvotes"])
         if upvotes == 0 and downvotes == 0:
-            return "No votes yet"
+            return MESSAGES["NO_VOTES"]
         elif upvotes == 0:
-            return f"{downvotes} downvotes"
+            return MESSAGES["VOTES_DOWN"].format(downvotes)
         elif downvotes == 0:
-            return f"{upvotes} upvotes"
+            return MESSAGES["VOTES_UP"].format(upvotes)
         else:
-            return f"{upvotes} upvotes, {downvotes} downvotes"
+            return MESSAGES["VOTES"].format(upvotes, downvotes)
+
+example = MergeRequest()
+print(example.status())
+example.vote("user1", "upvote")
+example.vote("user2", "upvote")
+example.vote("user3", "downvote")
+print(example.getvotes())
+print(example.status())
+print(example.close())
